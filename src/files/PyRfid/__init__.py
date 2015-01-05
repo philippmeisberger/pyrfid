706acb029989e628e0258c5e43cda77a7ca1fb34
Philipp Meisberger <team@pm-codeworks.de>
2015-01-05 17:35:15 +0100

    Moved files into PyRfid directory

---
+++ src/files/PyRfid/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Python RFID
+@author Philipp Meisberger <team@pm-codeworks.de>
+
+Copyright 2014 Philipp Meisberger
+All rights reserved.
+"""
+
+__version__ = '1.0'
+__copyright__ = 'Philipp Meisberger'
+__organization__ = 'PM Code Works'
+__license__ = 'BSD 3 License'
+__authors__ = ['Philipp Meisberger']
