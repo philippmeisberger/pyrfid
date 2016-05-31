c67c74e084ca15d5046827763293c260bfa4e278
Philipp Meisberger <team@pm-codeworks.de>
2016-05-31 20:50:24 +0200

    Example should now be compatible to Python3

--- src/files/examples/example-read.py
+++ src/files/examples/example-read.py
@@ -15,21 +15,21 @@ from pyrfid.pyrfid import PyRfid
 rfid = PyRfid('/dev/ttyUSB0', 9600)
 
 try:
-    print 'Waiting for tag...\n'
+    print('Waiting for tag...\n')
 
     if ( rfid.readTag() != True ):
         raise Exception('User aborted!')
 
-    print 'RAW:      '+ rfid.rawTag
-    print 'Checksum: '+ rfid.tagChecksum +'\n'
+    print('RAW:      '+ rfid.rawTag)
+    print('Checksum: '+ rfid.tagChecksum +'\n')
 
-    print 'Decimal-format:'
-    print 'ID:       '+ rfid.tagId
-    print 'Type:     '+ rfid.tagType +'\n'
+    print('Decimal-format:')
+    print('ID:       '+ rfid.tagId)
+    print('Type:     '+ rfid.tagType +'\n')
 
-    print 'Float-format:'
-    print 'ID:       '+ rfid.tagIdFloat
-    print 'Type:     '+ rfid.tagTypeFloat
+    print('Float-format:')
+    print('ID:       '+ rfid.tagIdFloat)
+    print('Type:     '+ rfid.tagTypeFloat)
 
 except Exception as e:
     print('[Exception] '+ str(e))
