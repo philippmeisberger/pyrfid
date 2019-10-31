7a963cc6a4c8980620c8212e3d1633528c905035
Philipp Meisberger <team@pm-codeworks.de>
2019-10-31 11:56:10 +0100

    Updated setup.py

--- src/setup.py
+++ src/setup.py
@@ -12,10 +12,12 @@ setup(
     name            = 'pyrfid',
     version         = pyrfid.__version__,
     description     = 'Python written library for an 125kHz RFID reader',
+    long_description= 'PyRfid is a Python written library for an 125kHz RFID reader. It was developed with the RDM6300 RFID-sensor which uses the EM4100 protocol. See the example-read.py for an example of how to use this library.',
     author          = 'Philipp Meisberger',
     author_email    = 'team@pm-codeworks.de',
-    url             = 'http://www.pm-codeworks.de/pyrfid.html',
+    url             = 'https://www.pm-codeworks.de/pyrfid.html',
     license         = 'D-FSL',
     package_dir     = {'': 'files'},
     packages        = ['pyrfid'],
+    install_requires = ['pyserial'],
 )
