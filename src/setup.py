9010070464785319a24de26a820aba0fbd717f32
Philipp Meisberger <team@pm-codeworks.de>
2019-11-04 21:46:56 +0100

    Added classifiers to setup.py

--- src/setup.py
+++ src/setup.py
@@ -15,9 +15,19 @@ setup(
     long_description= 'PyRfid is a Python written library for an 125kHz RFID reader. It was developed with the RDM6300 RFID-sensor which uses the EM4100 protocol. See the example-read.py for an example of how to use this library.',
     author          = 'Philipp Meisberger',
     author_email    = 'team@pm-codeworks.de',
-    url             = 'https://www.pm-codeworks.de/pyrfid.html',
+    url             = 'https://github.com/philippmeisberger/pyrfid',
     license         = 'D-FSL',
     package_dir     = {'': 'files'},
     packages        = ['pyrfid'],
     install_requires = ['pyserial'],
+    classifiers     = [
+        'Development Status :: 5 - Production/Stable'
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Terminals :: Serial',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
